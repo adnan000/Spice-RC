7a7020255b222cb9e9cb0cc0da533362b56041af
adnan000 <63094828+adnan000@users.noreply.github.com>
2020-04-08 22:29:34 +0700

    Insert all function needed in main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    return 0;
+}
+
+
+int* netlistToMatrix(char *netlist);
+
+int V_DC_node(); 
+
+int I_DC_branch();
+
+void printCSV();
+
+void printGraph();
