2b9399179dfb82aa75b34444f9f57e7394cd3402
meliafatimah <60214268+meliafatimah@users.noreply.github.com>
2020-04-09 19:09:23 +0700

    + main and struct

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct comp{
+    char kind;
+    float val;
+    int con1, con2;
+}comp;
+
+comp c;
+int ncomp;
+
+
 int main(){
 
-    return 0;
-}
+    printf("jumlah komponen : ");
+    scanf("%d", &ncomp);
+
+    comp arr_c[ncomp];
+    int i;
+
+    for (i = 0; i<ncomp ; i++){
+        printf("\nMasukkan komponen ke-%d\n", i+1);
+        printf("jenis : ");
+        scanf(" %c", &arr_c[i].kind);
+        printf("nilai : ");
+        scanf(" %f", &arr_c[i].val);
+        printf("koneksi : ");
+        scanf(" %d %d", &arr_c[i].con1, &arr_c[i].con2);
+    }
 
 
 int* netlistToMatrix(char *netlist);
